f614b983dd081d808c70a69a1e103b1a7b1c2084
Akshay Singh <akshaysingh@192.168.1.115>
2026-04-30 14:02:38 +0100

    Swap hero image positions: trading desk left, London right

--- src/components/HomeCasebook.tsx
+++ src/components/HomeCasebook.tsx
@@ -124,8 +124,8 @@ export function HomeCasebook({
             </div>
 
             <div className="grid grid-cols-12 gap-3">
-              <MediaTile image={heroImages[0]} className="col-span-8 h-44 md:h-52" failedImages={failedImages} setFailedImages={setFailedImages} />
-              <MediaTile image={heroImages[1]} className="col-span-4 h-44 md:h-52" failedImages={failedImages} setFailedImages={setFailedImages} />
+              <MediaTile image={heroImages[1]} className="col-span-8 h-44 md:h-52" failedImages={failedImages} setFailedImages={setFailedImages} />
+              <MediaTile image={heroImages[0]} className="col-span-4 h-44 md:h-52" failedImages={failedImages} setFailedImages={setFailedImages} />
               <div className="coverage-panel col-span-12">
                 <div className="flex items-center justify-between border-b border-border px-4 py-3">
                   <p className="section-label">Coverage Panel</p>
